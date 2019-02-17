07f8b07289ba554772dd3d531c182cf2b1c98f23
Andrei <ovidiu.andrey97@gmail.com>
2019-02-17 13:00:14 +0000

    associative data structure

--- w05/associative.c
+++ w05/associative.c
@@ -39,33 +39,73 @@ void insert(struct DataItem **hashArray, int key, int data)
     int hashIndex = hashCode(key);
     while(hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1)
     {
-    printf("%d\n", hashArray[hashIndex]);
+    //printf("%d\n", hashArray[hashIndex]);
         ++hashIndex;
         hashIndex %= SIZE;
     }
     hashArray[hashIndex] = item;
 }
 
+struct DataItem* search(struct DataItem **hashArray, int key)
+{
+    int hashIndex = hashCode(key);
+    while(hashArray[hashIndex] != NULL)
+    {
+        if (hashArray[hashIndex]->key == key) 
+        {
+            return hashArray[hashIndex];
+        }
+        hashIndex++;
+        hashIndex %= SIZE;
+        
+    }
+    return NULL;
+}
+struct DataItem* delete(struct DataItem **hashArray, struct DataItem* item)
+{
+    int key = item->key;
+    int hashIndex = hashCode(key);
 
-
+    while(hashArray[hashIndex] != NULL)
+    {
+        if(hashArray[hashIndex]->key == key)
+        {
+            struct DataItem *temp = hashArray[hashIndex];
+            hashArray[hashIndex] = NULL;
+            return temp;
+        }
+        hashIndex++;
+        hashIndex %= SIZE;
+    }
+    return NULL;
+}
 
 
 
 int main()
 {
-    struct DataItem** hArray= malloc(SIZE * sizeof **hArray);
-    for (int i = 0; i < SIZE; ++i)
-        printf("%d\n", hArray[i]);
+    struct DataItem** hArray= calloc(SIZE, sizeof **hArray);
+    struct DataItem *tempItem = (struct DataItem*) malloc(sizeof(struct DataItem));
+    tempItem->data = -1;
+    tempItem->key = -1;
     
-    //struct DataItem *tempItem = (struct DataItem*) malloc(sizeof(struct DataItem));
-    //tempItem->data = -1;
-    //tempItem->key = -1;
+    display(hArray);
+    insert(hArray,1,20);
+    insert(hArray,2,30);
+    insert(hArray,42,79);
+    insert(hArray,4,25);
+    insert(hArray,0,25);
+    display(hArray);
+    tempItem = search(hArray,42);
+    if(tempItem != NULL)
+    {
+        printf("elem found: %d\n", tempItem->data);
+    }else
+    {
+        printf("nope");
+    }
+    tempItem = delete(hArray, tempItem);
+    display(hArray);
     
-    //display(hArray);
-   insert(hArray,1,20);
-    //insert(hArray,2,30);
-    //insert(hArray,42,79);
-   // insert(hArray,4,25);
-   display(hArray);
     return 0;
 }
